bb9261dbd34fc04fe210ad8b9bbe6b1d75dfe9ff
yeahsilver <gjdpdms2005@gmail.com>
2023-11-13 13:36:40 +0900

    addons 추가

--- lib/widgetbook.dart
+++ lib/widgetbook.dart
@@ -20,7 +20,24 @@ class WidgetbookApp extends StatelessWidget {
     return Widgetbook.material(
       // Use the generated directories variable
       directories: directories,
-      addons: [],
+      addons: [
+        DeviceFrameAddon(
+          devices: [
+            Devices.ios.iPhoneSE,
+            Devices.ios.iPhone13ProMax,
+            Devices.ios.iPadPro11Inches,
+            Devices.android.samsungGalaxyA50,
+          ],
+        ),
+        AlignmentAddon(),
+        LocalizationAddon(locales: [
+          const Locale('en', 'US'),
+          const Locale('ko', 'KR'),
+        ], localizationsDelegates: [
+          DefaultWidgetsLocalizations.delegate,
+          DefaultMaterialLocalizations.delegate
+        ])
+      ],
       integrations: [
         // To make addons & knobs work with Widgetbook Cloud
         WidgetbookCloudIntegration(),
